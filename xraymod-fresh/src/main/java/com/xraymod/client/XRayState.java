b1038f6243ea39f18f9a6bcce1818f9893ddcc08
NotKT <notkt8130@gmail.com>
2026-05-24 00:59:22 +0530

    Update XRayState.java

--- xraymod-fresh/src/main/java/com/xraymod/client/XRayState.java
+++ xraymod-fresh/src/main/java/com/xraymod/client/XRayState.java
@@ -5,6 +5,7 @@ import com.xraymod.client.config.XRayConfig;
 public final class XRayState {
     private XRayState() {}
     public static volatile boolean active = false;
+    public static volatile boolean entityGlowActive = false;
     public static XRayConfig config;
     public static volatile int playerChunkX = 0;
     public static volatile int playerChunkZ = 0;
